9402f858e703f7ae7f15f739861ca1e74566b694
Cassidy Brooke <cbrooke@Cassidys-MBP.pcc.com>
2018-06-13 10:46:23 -0400

    Fossil depot scaffold

---
+++ db/seeds.rb
@@ -0,0 +1,32 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+
+Product.delete_all
+
+Product.create!(title: 'Ammonite Fossil',
+                description:
+                    %{<p>Ammonites
+An ammonite fossil cut in half to reveal inner structure. From ammonites for sale on FossilEra.com were predatory mollusks that resembled squid. These cephalopods had eyes, tentacles, and spiral shells. They are more closely related to a living octopus, though the shells resemble that of a nautilus. Ammonites first appearing in the fossil record 240 million years ago, descending from straight shelled cephalopods. The last lineages disappeared 65 million years ago at the end of the Cretaceous. </p>},
+                image_url: 'fossil.jpg',
+                price: 1350.00)
+
+Product.create!(title: 'Ammonite Fossil',
+                description:
+                    %{<p>Ammonites
+An ammonite fossil cut in half to reveal inner structure. From ammonites for sale on FossilEra.com were predatory mollusks that resembled squid. These cephalopods had eyes, tentacles, and spiral shells. They are more closely related to a living octopus, though the shells resemble that of a nautilus. Ammonites first appearing in the fossil record 240 million years ago, descending from straight shelled cephalopods. The last lineages disappeared 65 million years ago at the end of the Cretaceous. </p>},
+                image_url: 'fossil.jpg',
+                price: 1350.00)
+
+Product.create!(title: 'Ammonite Fossil',
+                description:
+                    %{<p>Ammonites
+An ammonite fossil cut in half to reveal inner structure. From ammonites for sale on FossilEra.com were predatory mollusks that resembled squid. These cephalopods had eyes, tentacles, and spiral shells. They are more closely related to a living octopus, though the shells resemble that of a nautilus. Ammonites first appearing in the fossil record 240 million years ago, descending from straight shelled cephalopods. The last lineages disappeared 65 million years ago at the end of the Cretaceous. </p>},
+                image_url: 'fossil.jpg',
+                price: 1350.00)
+
